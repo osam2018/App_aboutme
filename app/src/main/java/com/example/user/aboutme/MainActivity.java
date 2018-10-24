0fc31d96e10dd42e85e87157705574708ba45eea
Changdeok-Hong <wonderkyle@naver.com>
2018-10-24 10:59:35 +0900

    Third Commit

--- app/src/main/java/com/example/user/aboutme/MainActivity.java
+++ app/src/main/java/com/example/user/aboutme/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.user.aboutme;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private long lastTimeBackPressed; //뒤로가기 버튼 터치 시각
+    ConstraintLayout container;
     HomeFragment homeFragment;
     HistoryFragment historyFragment;
     SettingsFragment settingsFragment;
@@ -46,16 +54,33 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this, SplashView.class));
 
         //Fragment
+        container = (ConstraintLayout) findViewById(R.id.container);
         homeFragment = (HomeFragment) getSupportFragmentManager().findFragmentById(R.id.homeFragment);
         historyFragment = new HistoryFragment();
         settingsFragment = new SettingsFragment();
+
+        Button writingbutton = (Button) findViewById(R.id.writingbutton) ;
+        writingbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.fragment_writing, container, true);
+            }
+        });
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-
-
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() - lastTimeBackPressed < 1500) {
+            finish();
+            return;
+        }
+        Toast.makeText(this, "'뒤로' 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        lastTimeBackPressed = System.currentTimeMillis();
+    }
 
 }
